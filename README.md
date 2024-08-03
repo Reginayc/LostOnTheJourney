0d004491cd8b9e455d43b64d9bf979e8cbbab7b7
Regina <regina930827@gmail.com>
2024-08-04 09:48:12 +1200

    add readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# LostOnTheJourney
+
+## Description
+
+LostOnTheJourney is a full-stack movie application built with a C# backend and a React frontend. The application allows users to view, add, and manage a list of movies.
+
+## Features
+
+- **List Movies:** View a list of movies with their details.
+- **Add Movies:** Add new movies to the list.
+- **Update Movies:** Edit existing movie details.
+- **Delete Movies:** Remove movies from the list.
+
+## Installation
+
+To get started with the project, follow these steps:
+
+1. **Clone the Repository:**
+
+   ```bash
+   git clone https://github.com/Reginayc/LostOnTheJourney.git
--- obj/Debug/net6.0/LostOnTheJourney.Api.AssemblyInfo.cs
+++ obj/Debug/net6.0/LostOnTheJourney.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LostOnTheJourney.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbd21afbd04572a413cf397e395d7fc027a8aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("LostOnTheJourney.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LostOnTheJourney.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
